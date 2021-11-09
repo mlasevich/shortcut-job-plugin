a0221d1a872d4b774d8e61464d5b5eae61a5ec37
Michael "M3" Lasevich <michael@legrig.com>
2021-11-09 11:01:39 -0800

    add an initial unit test

--- src/test/java/com/legrig/jenkins/shortcut/ShortcutJobTest.java
+++ src/test/java/com/legrig/jenkins/shortcut/ShortcutJobTest.java
@@ -1,11 +1,20 @@
 package com.legrig.jenkins.shortcut;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.junit.Assert.assertFalse;
 
 public class ShortcutJobTest {
 
     @Rule
     public JenkinsRule jenkins = new JenkinsRule();
+
+    @Test
+    public void notConfiguredTest() throws Exception {
+        ShortcutJob job = jenkins.createProject(ShortcutJob.class);
+        assertFalse(job.isConfigured());
+    }
 
 }
